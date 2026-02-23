68084c13422aff0e7893ff40a4c033e1d3c2db24
Vishal <thevishaleditor@gmail.com>
2026-02-23 21:15:15 +0530

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,35 @@
+// let a = 5;
+// let b = 4;
+// let c="5";
+
+
+// console.log("a+b");
+// console.log("a+b" , a+b);
+// console.log("a-b" , a-b);
+// console.log("a*b" , a*b);
+
+
+// console.log("a =",a,"& b =",b);
+// a++;
+// console.log("a",a);
+// console.log("a",++a);
+// console.log("a",a++);
+// console.log("a",a);
+
+
+// console.log("5===4",a===b);
+// console.log("5===5",a===c);
+// console.log("5!=4",a!=b);
+
+// console.log("5==4",a==b);
+// console.log("5==5",a==c);
+// console.log("5!=4",a!=b);
+
+
+let a= 6;
+let b= 5;
+
+let cond1 = a>b ;
+let cond2 = a=== 6;
+
+console.log("cond1 && cond2",cond1 && cond2);
